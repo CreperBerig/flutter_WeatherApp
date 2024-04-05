ea7df722275a4468b4687d7c1d9a1d9b8b120162
CreperBerig <rcreper135@gmail.com>
2024-04-05 14:32:07 +0300

    Create weather model and weather sirvice

--- lib/features/weather_screen/viev/current_weather_screen.dart
+++ lib/features/weather_screen/viev/current_weather_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:simple_weather_app/repositores/weather_modul.dart';
+
+import '../../../repositores/weather_servis.dart';
 
 class CurrentWeatherScreen extends StatefulWidget {
   const CurrentWeatherScreen({super.key});
@@ -8,8 +11,29 @@ class CurrentWeatherScreen extends StatefulWidget {
 }
 
 class _CurrentWeatherScreenState extends State<CurrentWeatherScreen> {
+  late Future<WeatherResponse> _weather;
+  @override
+  void initState() {
+    _weather = WeatherServis().fetchWeather();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: Text('Weather app'),),
+      body: Center(
+        child: FutureBuilder(
+          future: _weather,
+          builder: (context, snapshot) {
+            if(snapshot.hasData){
+              return Text('Данные полученны');
+            } else {
+              return CircularProgressIndicator();
+            }
+          },
+        ),
+      )
+    );
   }
 }
---
+++ lib/repositores/weather_servis.dart
@@ -0,0 +1,25 @@
+import 'package:dio/dio.dart';
+
+import 'weather_modul.dart';
+
+class WeatherServis{
+  final Dio _dio = Dio();
+
+  Future<WeatherResponse> fetchWeather() async{
+    try{
+      final response = await _dio.get('https://api.openweathermap.org/data/2.5/weather', queryParameters: {
+        'lat': 53.20,
+        'lon': 45.00,
+        'lang': 'ru',
+        'units': 'metric',
+        'appid': '4aa1ca0b12e06de913eeb23d2b07a0d9',
+      });
+
+      return WeatherResponse.fromJson(response.data);
+    } catch (error) {
+      throw Exception("Failed to load data: $error");
+    }
+  }
+}
+
+//lat=53.20&lon=45.00&lang=ru&units=metric&appid=4aa1ca0b12e06de913eeb23d2b07a0d9
